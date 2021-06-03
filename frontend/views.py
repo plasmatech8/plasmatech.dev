9055945c9c3e5194b12d56897241cb3f44074d72
plasmatech8 <com0004@gmail.com>
2021-06-03 16:37:37 +1000

    Initialised frontend urls & template

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request, *args, **kwargs):
+    return render(request, 'index.html')
